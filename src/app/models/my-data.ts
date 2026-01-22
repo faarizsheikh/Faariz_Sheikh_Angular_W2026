4ea6b68339ded90523f885600ec0edcc6d669530
Faariz Sheikh <faarizyt@gmail.com>
2026-01-22 10:33:41 -0500

    Added: - CSS / SCSS styling. - Initialized films array of objects. - New variables. - Status toggle button to toggle between "Not Started", "Watching", and "Finished".

--- src/app/models/my-data.ts
+++ src/app/models/my-data.ts
@@ -1,24 +1,29 @@
 export interface ExtraInfo {
-  based_on ?: string;
-  creator ?: string;
-  notes ?: string;
-  sequentialNumbering ?: number;
+  based_on?: string;
+  creator?: string;
+  movie_sequence?: number;
+  mpaa_rating?: string;
+  notes?: string;
+  seasons?: number;
+  total_episodes?: number;
+  tv_rating?: string;
 }
 
 export interface MyData extends ExtraInfo {
-  // My topic is films, not video games (a topic chosen previous semester in Angular class :> )
+  // My topic is TV Shows and Movies, NOT video games (a topic chosen previous semester in Angular class :> )
   id: number;
-  title : string;
-  description : string;
-  genre : string;
-  yearReleased : number;
-  mpaa_rating : string;
-  viewer_rating ?: number;
-  streaming : string;
-  producer : string;
-  director : string;
-  writer : string
-  starring : string;
-  is_watched : boolean;
-  image_url ?: string; /* I know we will need this to be mandatory later. */
+  title: string;
+  description: string;
+  type: string;
+  genre: string;
+  yearReleased: number;
+  viewer_rating?: number;
+  streaming: string;
+  producer: string;
+  director: string;
+  writer: string
+  starring: string;
+  is_started: boolean;
+  is_finished: boolean;
+  image_url?: string; /* I know we will need this to be mandatory later. */
 }
