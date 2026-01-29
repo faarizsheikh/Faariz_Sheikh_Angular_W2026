c5e9240262b66258e6eed1948f2bea3df5777269
Faariz Sheikh <faarizyt@gmail.com>
2026-01-28 22:27:14 -0500

    UPDATED: film-list-item to show each item in array with headings and paragraph tags.

--- src/app/film-list-item/film-list-item.ts
+++ src/app/film-list-item/film-list-item.ts
@@ -1,14 +1,42 @@
 import { Component, Input } from '@angular/core';
 import { MyData } from '../models/my-data';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-film-list-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './film-list-item.html',
   styleUrls: ['./film-list-item.css'],
 })
 
 export class FilmListItem {
   @Input() film!: MyData;
+
+  // This will get the current status:
+  Status(film?: MyData): 'not-started' | 'watching' | 'finished' {
+    if (!film || (!film.is_started && !film.is_finished)) return 'not-started';
+    if (film.is_started && !film.is_finished) return 'watching';
+    return 'finished';
+  }
+
+  // This will be used for changing the status
+  toggleStatus(film?: MyData): void {
+    if (!film) return;
+
+    const status = this.Status(film);
+
+    if (status === 'not-started') {
+      film.is_started = true;
+      film.is_finished = false;
+    }
+    else if (status === 'watching') {
+      film.is_started = true;
+      film.is_finished = true;
+    }
+    else {
+      film.is_started = false;
+      film.is_finished = false;
+    }
+  }
 }
