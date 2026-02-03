d6c42e8f1b169ee55f957c7a97fd7477e06823d0
Faariz Sheikh <faarizyt@gmail.com>
2026-02-02 23:14:36 -0500

    ADDED: List of routes in app.routes.ts & Set-up for routes in main.ts.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 // app.routes.ts:
 
 import { Routes } from '@angular/router';
+import { FilmDetails } from './film-details/film-details';
+import { FilmList } from './film-list/film-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/films', pathMatch: 'full' },
+  { path: 'films', component: FilmList },
+  { path: 'films/:id', component: FilmDetails },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 // main.ts:
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { App } from './app/app';
-import { appConfig } from './app/app.config';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)]
+}).catch(err => console.error(err));
